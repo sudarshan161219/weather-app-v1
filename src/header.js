066dd0e9cac8230201bd9525b000035241fad823
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2022-12-15 19:26:50 +0530

    Wen, Dec 15/22

--- src/header.js
+++ src/header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import gif from './images/weathercock.gif'
+import {BsGithub} from 'react-icons/bs'
+
 
 
 
@@ -8,9 +10,8 @@ const Header = () => {
  return ( 
  <nav className="nav">
 <h1 className='nav-heading' >Weather </h1>
-{/* <WiCloudyGusts className='nav-icon' /> */}
 <img className='nav-icon' src={gif} alt='gif'></img>
-
+<a href="https://github.com/sudarshan161219" > < BsGithub className='github-icon' /></a> 
  </nav>
       );
     };
